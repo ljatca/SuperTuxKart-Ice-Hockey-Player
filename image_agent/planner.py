c7bceb86cd470db44f9f088457a279b63d61d6c7
Jing Liu <j762liu@uwaterloo.ca>
2023-11-21 18:25:35 -0500

    Reverted model

--- image_agent/planner.py
+++ image_agent/planner.py
@@ -1,5 +1,4 @@
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 
 
@@ -9,50 +8,47 @@ def spatial_argmax(logit):
     :param logit: A tensor of size BS x H x W
     :return: A tensor of size BS x 2 the soft-argmax in normalized coordinates (-1 .. 1)
     """
-
-    # print("spatial_argmax(logit): ", logit.size())
+    
     weights = F.softmax(logit.view(logit.size(0), -1), dim=-1).view_as(logit)
-    # print("weights", weights.size())
     return torch.stack(((weights.sum(1) * torch.linspace(-1, 1, logit.size(2)).to(logit.device)[None]).sum(1),
                         (weights.sum(2) * torch.linspace(-1, 1, logit.size(1)).to(logit.device)[None]).sum(1)), 1)
 
+# Directly use the CNNClassifier model from HW3 solution
+class CNNClassifier(torch.nn.Module):
+    class Block(torch.nn.Module):
+        def __init__(self, n_input, n_output, kernel_size=3, stride=2):
+            super().__init__()
+            self.c1 = torch.nn.Conv2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
+                                      stride=stride, bias=False)
+            self.c2 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2, bias=False)
+            self.c3 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2, bias=False)
+            self.b1 = torch.nn.BatchNorm2d(n_output)
+            self.b2 = torch.nn.BatchNorm2d(n_output)
+            self.b3 = torch.nn.BatchNorm2d(n_output)
+            self.skip = torch.nn.Conv2d(n_input, n_output, kernel_size=1, stride=stride)
 
+        def forward(self, x):
+            return F.relu(self.b3(self.c3(F.relu(self.b2(self.c2(F.relu(self.b1(self.c1(x)))))))) + self.skip(x))
+
+    def __init__(self, layers=[16, 32, 64, 128], n_output_channels=6, kernel_size=3):
+        super().__init__()
+        self.input_mean = torch.Tensor([0.3235, 0.3310, 0.3445])
+        self.input_std = torch.Tensor([0.2533, 0.2224, 0.2483])
+
+        L = []
+        c = 3
+        for l in layers:
+            L.append(self.Block(c, l, kernel_size, 2))
+            c = l
+        self.network = torch.nn.Sequential(*L)
+        self.classifier = torch.nn.Linear(c, n_output_channels)
+
+    def forward(self, x):
+        z = self.network((x - self.input_mean[None, :, None, None].to(x.device)) / self.input_std[None, :, None, None].to(x.device))
+        return self.classifier(z.mean(dim=[2, 3]))
 
 # Directly use the Detector model from HW4 solution
 class Planner(torch.nn.Module):
-    class CNNClassifier(torch.nn.Module):
-        class Block(torch.nn.Module):
-            def __init__(self, n_input, n_output, kernel_size=3, stride=2):
-                super().__init__()
-                self.c1 = torch.nn.Conv2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
-                                          stride=stride, bias=False)
-                self.c2 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2, bias=False)
-                self.c3 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2, bias=False)
-                self.b1 = torch.nn.BatchNorm2d(n_output)
-                self.b2 = torch.nn.BatchNorm2d(n_output)
-                self.b3 = torch.nn.BatchNorm2d(n_output)
-                self.skip = torch.nn.Conv2d(n_input, n_output, kernel_size=1, stride=stride)
-
-            def forward(self, x):
-                return F.relu(self.b3(self.c3(F.relu(self.b2(self.c2(F.relu(self.b1(self.c1(x)))))))) + self.skip(x))
-
-        def __init__(self, layers=[16, 32, 64, 128], n_output_channels=6, kernel_size=3):
-            super().__init__()
-            self.input_mean = torch.Tensor([0.3235, 0.3310, 0.3445])
-            self.input_std = torch.Tensor([0.2533, 0.2224, 0.2483])
-
-            L = []
-            c = 3
-            for l in layers:
-                L.append(self.Block(c, l, kernel_size, 2))
-                c = l
-            self.network = torch.nn.Sequential(*L)
-            self.classifier = torch.nn.Linear(c, n_output_channels)
-
-        def forward(self, x):
-            z = self.network((x - self.input_mean[None, :, None, None].to(x.device)) / self.input_std[None, :, None, None].to(x.device))
-            return self.classifier(z.mean(dim=[2, 3]))
-
     class UpBlock(torch.nn.Module):
         def __init__(self, n_input, n_output, kernel_size=3, stride=2):
             super().__init__()
@@ -67,26 +63,18 @@ class Planner(torch.nn.Module):
         self.input_mean = torch.Tensor([0.2788, 0.2657, 0.2629])
         self.input_std = torch.Tensor([0.2064, 0.1944, 0.2252])
 
-        self.convs = nn.ModuleList()
-        self.upconvs = nn.ModuleList()
         c = 3
         self.use_skip = use_skip
-
         self.n_conv = len(layers)
         skip_layer_size = [3] + layers[:-1]
-
         for i, l in enumerate(layers):
-            conv_block = self.CNNClassifier.Block(c, l, kernel_size, 2)
-            self.convs.append(conv_block)
+            self.add_module('conv%d' % i, CNNClassifier.Block(c, l, kernel_size, 2))
             c = l
-
         for i, l in list(enumerate(layers))[::-1]:
-            upconv_block = self.UpBlock(c, l, kernel_size, 2)
-            self.upconvs.append(upconv_block)
+            self.add_module('upconv%d' % i, self.UpBlock(c, l, kernel_size, 2))
             c = l
             if self.use_skip:
                 c += skip_layer_size[i]
-
         self.classifier = torch.nn.Conv2d(c, n_output_channels, 1)
 
     def forward(self, img):
@@ -98,29 +86,19 @@ class Planner(torch.nn.Module):
         """
         z = (img - self.input_mean[None, :, None, None].to(img.device)) / self.input_std[None, :, None, None].to(img.device)
         up_activation = []
-
-        for i, conv in enumerate(self.convs):
+        for i in range(self.n_conv):
             # Add all the information required for skip connections
             up_activation.append(z)
-            z = conv(z)
+            z = self._modules['conv%d'%i](z)
 
-
-        #print("Upconv layers before reversal:")
-        #for upconv in self.upconvs:
-        #    print(upconv)
-
-        i = 3
-        for upconv in self.upconvs:
-            #print(i, upconv)
-            #print(z.size())
-            z = upconv(z)
+        for i in reversed(range(self.n_conv)):
+            z = self._modules['upconv%d'%i](z)
             # Fix the padding
             z = z[:, :, :up_activation[i].size(2), :up_activation[i].size(3)]
             # Add the skip connection
             if self.use_skip:
                 z = torch.cat([z, up_activation[i]], dim=1)
-            i -= 1
-
+                
         # print("z: ", z.size())
         z = self.classifier(z)
         # print("z.squeeze(1): ", z.size())
@@ -130,15 +108,13 @@ def save_model(model):
     from torch import save
     from os import path
     if isinstance(model, Planner):
-        model_scripted = torch.jit.script(model)
-        return model_scripted.save('planner.pt')
+        return save(model.state_dict(), path.join(path.dirname(path.abspath(__file__)), 'planner.pt'))
     raise ValueError("model type '%s' not supported!" % str(type(model)))
 
 
 def load_model():
     from torch import load
     from os import path
-
-    model = load(path.join(path.dirname(path.abspath(__file__)), 'planner.pt'))
-    return model
-
+    r = Planner()
+    r.load_state_dict(load(path.join(path.dirname(path.abspath(__file__)), 'planner.pt'), map_location='cpu'))
+    return r
