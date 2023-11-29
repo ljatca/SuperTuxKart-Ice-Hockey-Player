3c6f0de1ffa8d38357a9c7446162f30fcad691c8
Jing Liu <j762liu@uwaterloo.ca>
2023-11-29 01:06:54 -0500

    Added player control

--- image_agent/player.py
+++ image_agent/player.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 from .utils import save_image
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
+def norm(vector):
+    return np.linalg.norm(vector)
+
 class Team:
     agent_type = 'image'
 
@@ -19,6 +22,8 @@ class Team:
         self.model = load_model()
         self.num_players = None
         self.transform = TF.to_tensor
+        self.actions = [{'acceleration': 0, 'brake': False, 'drift': False, 'nitro': False, 'rescue': False, 'steer': 0},
+                        {'acceleration': 0, 'brake': False, 'drift': False, 'nitro': False, 'rescue': False, 'steer': 0}]
 
     def new_match(self, team: int, num_players: int) -> list:
         """
@@ -71,44 +76,104 @@ class Team:
                  rescue:       bool (optional. no clue where you will end up though.)
                  steer:        float -1..1 steering angle
         """
-
-        ret = []
-        acceleration = 1
+        GOALS = np.float32([[0, 64.5], [0, -64.5]])
 
         for i, img in enumerate(player_image):
-            plt.imsave(f'player_image_{i}.png', img)
-            image = self.transform(player_image[i])
+            action = self.actions[i]
+            image = self.transform(img)
             pred = self.model(image).cpu().detach().numpy()
 
-            puck_locaton = pred[0][0]
-            print("puck", puck_locaton)
-            save_image(image, pred)
+            puck_location_x = pred[0,0]
+            puck_location_y = pred[0,1]
 
-            player= player_state[i]
-            front = np.array(player['kart']['front'])[[0]]
-            loc = np.array(player['kart']['location'])[[0]]
-            velocity = np.array(player['kart']['velocity'])
-            velocity = np.linalg.norm(velocity)
-            print("velocity: ", velocity)
-            if velocity >= 5:
-                acceleration = 0
+            
+            player = player_state[i]
+
+            kart_front = np.array(player['kart']['front'])[[0, 2]]
+            kart_location = np.array(player['kart']['location'])[[0, 2]]
+
+            dir = kart_front - kart_location
+            dir = dir / norm(dir)
+
+            # print("dir: ", dir)
+
+
+            goal_dir = GOALS[self.team - 1] - kart_location
+            # print("goal_dir: ", goal_dir)
+
+            goal_dir = goal_dir / norm(goal_dir)
+
+            goal_angle = np.arccos(np.clip(np.dot(dir, goal_dir), -1, 1))
+
+            goal_dir = GOALS[self.team - 1] - kart_location
+            goal_dist = norm(goal_dir)
+            goal_dir = goal_dir / norm(goal_dir)
+
+            goal_angle = np.arccos(np.clip(np.dot(dir, goal_dir), -1, 1))
+            signed_goal_angle = np.degrees(
+                -np.sign(np.cross(dir, goal_dir)) * goal_angle)
+
+            goal_dist = (
+                (np.clip(goal_dist, 10, 100) - 10) / 90) + 1
+            
+            MIN_ANGLE = 20
+            MAX_ANGLE = 120
+
+            puck_loc = pred[0][0]
+
+            # print("puck_loc: ", puck_loc)
+            if MIN_ANGLE < np.abs(signed_goal_angle) < MAX_ANGLE:
+                distW = 1 / goal_dist ** 3
+                aim_point = puck_loc + \
+                    np.sign(puck_loc - signed_goal_angle /
+                            100) * 0.3 * distW                
             else:
-                acceleration = 1
+                aim_point = puck_loc
+              
+            
+            kart_velocity = np.array(player['kart']['velocity'])
 
+            kart_velocity = np.linalg.norm(kart_velocity)
+            # print("kart_velocity: ", kart_velocity)
+            
+            target_velocity = 15
 
-            ret.append(dict(acceleration=acceleration, steer=puck_locaton))
+            # Use a proportional controller for steering
+            desired_steer = aim_point * 2.0
 
+            # Limit the steering to avoid extreme values
+            desired_steer = max(-1, min(1, desired_steer))
 
+            # Reduce speed if a sharp turn is detected
+            if abs(desired_steer) > 0.8:
+                target_velocity = 8
 
+            velocity_difference = target_velocity - kart_velocity
 
+            max_acceleration = 0.8 
+            brake_threshold = 0.5
+            drift_threshold = 0.3 
 
+            # Use a proportional controller for acceleration
+            if velocity_difference > 0:
+                action["acceleration"] = min(1, max(velocity_difference / 10, max_acceleration))
+                action["brake"] = False
+            else:
+                # Use brake if slowing down too fast
+                if kart_velocity < brake_threshold * target_velocity:
+                    action["acceleration"] = 0.0
+                    action["brake"] = False
+                else:
+                    action["acceleration"] = 0.0
+                    action["brake"] = False
+            action["steer"] = desired_steer
 
-        direction = (front - loc)
-        print(direction)
-        direction /= np.linalg.norm(direction)
-
-
-
+            # Activate drift only when turning sharply
+            if abs(desired_steer) > drift_threshold:
+                action["drift"] = True
+            else:
+                action["drift"] = False
+        
         # TODO: Change me. I'm just cruising straight
-        return ret
+        return self.actions
         #return [dict(acceleration=0.5, steer=puck_locaton)] * self.num_players
